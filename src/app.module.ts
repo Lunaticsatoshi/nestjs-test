c00e87ed8bcdac95c0aa9f38ec856f3ba5c1f86d
Lunaticsatoshi <deysarkersudeep@gmail.com>
2023-03-03 20:11:55 +0530

    :tada: feat: creating user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,16 +3,18 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GameModule } from './game/game.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'gameRoom',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      entities: [__dirname + '/**/*.model{.ts,.js}'],
       synchronize: true,
     }),
     GameModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
