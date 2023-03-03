e351adba5e39143ebd92cc9450536ce1d0a8fb29
Lunaticsatoshi <deysarkersudeep@gmail.com>
2023-03-03 20:04:05 +0530

    :zap: chore: adding typeorm module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GameModule } from './game/game.module';
 
 @Module({
-  imports: [GameModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'gameRoom',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    GameModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
