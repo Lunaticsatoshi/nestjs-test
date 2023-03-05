e3ba90d88983e29519166138165e32fc8b8bcc77
Lunaticsatoshi <deysarkersudeep@gmail.com>
2023-03-05 12:50:12 +0530

    :zap: chore: updating port

--- src/main.ts
+++ src/main.ts
@@ -27,6 +27,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3006);
+  await app.listen(process.env.PORT || 3006);
 }
 bootstrap();
